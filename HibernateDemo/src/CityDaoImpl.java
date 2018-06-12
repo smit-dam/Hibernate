a21e9b264e79a0b088e3713566172ff7f52c3eec
Smitesh <smitesh_damdoo@rediffmail.com>
2018-06-12 08:02:05 +0530

    Demo for NamedQueries and NamedQuery. Need to be further updated for DAO implementation.

---
+++ HibernateDemo/src/CityDaoImpl.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class CityDaoImpl implements ICityDao{
+
+	@Override
+	public List getCityByName(String name) {
+		Map<Integer, String> cityMap = new HashMap<Integer,String>();
+		List cityList = new ArrayList();
+//		Query query = 
+ 		return null;
+	}
+
+	@Override
+	public List getCityByCircle(String circle) {
+		return null;
+	}
+
+}
